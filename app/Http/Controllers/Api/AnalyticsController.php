6243bb9187a3fba165c38a13da9e24563aeafd96
Ben-Piet O'Callaghan <bpocallaghan@gmail.com>
2020-09-30 13:09:13 +0200

    API - google analytics

---
+++ app/Http/Controllers/Api/AnalyticsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Analytics;
+use App\Models\User;
+use App\Http\Requests;
+use Spatie\Analytics\Period;
+use App\Http\Controllers\Traits\GoogleAnalyticsHelper;
+
+class AnalyticsController extends ApiController
+{
+    use GoogleAnalyticsHelper;
+
+    /**
+     * Get the sessions grouped by country
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getVisitorsLocations()
+    {
+        $start = input('start', date('Y-m-d', strtotime('-29 days')));
+        $end = input('end', date('Y-m-d'));
+
+        if (is_string($start)) {
+            $start = \DateTime::createFromFormat('Y-m-d', $start);
+        }
+
+        if (is_string($end)) {
+            $end = \DateTime::createFromFormat('Y-m-d', $end);
+        }
+
+        $period = Period::create($start, $end);
+
+        $data = Analytics::performQuery($period, 'ga:sessions', [
+            'dimensions'  => 'ga:country',
+            'sort'        => '-ga:sessions',
+            'max-results' => 50
+        ]);
+
+        $items = [];
+        if ($data->rows) {
+            $items = $data->rows;
+        }
+
+        foreach ($items as $k => $item) {
+            $items[$k][1] = intval($items[$k][1]);
+        }
+
+        return json_response($items);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AnalyticsController;
+use App\Http\Controllers\Api\NotificationsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// notifications
+Route::group(['prefix' => 'notifications',], function () {
+    Route::post('/{user}', [NotificationsController::class, 'index']);
+    Route::post('/{user}/unread', [NotificationsController::class, 'unread']);
+    Route::post('/{user}/read/{notification}', [NotificationsController::class, 'read']);
+
+    Route::post('/actions/latest', [NotificationsController::class, 'getLatestActions']);
+});
+
+// analytics
+Route::group(['prefix' => 'analytics'], function () {
+    Route::post('/keywords', [AnalyticsController::class, 'getKeywords']);
+    Route::post('/visitors', [AnalyticsController::class, 'getVisitors']);
+    Route::post('/browsers', [AnalyticsController::class, 'getBrowsers']);
+    Route::post('/referrers', [AnalyticsController::class, 'getReferrers']);
+    Route::post('/page-load', [AnalyticsController::class, 'getAvgPageLoad']);
+    Route::post('/bounce-rate', [AnalyticsController::class, 'getBounceRate']);
+    Route::post('/visited-pages', [AnalyticsController::class, 'getVisitedPages']);
+    Route::post('/active-visitors', [AnalyticsController::class, 'getActiveVisitors']);
+    Route::post('/unique-visitors', [AnalyticsController::class, 'getUniqueVisitors']);
+    Route::post('/visitors-views', [AnalyticsController::class, 'getVisitorsAndPageViews']);
+    Route::post('/visitors/locations', [AnalyticsController::class, 'getVisitorsLocations']);
+
+    Route::post('/age', [AnalyticsController::class, 'getUsersAge']);
+    Route::post('/devices', [AnalyticsController::class, 'getDevices']);
+    Route::post('/gender', [AnalyticsController::class, 'getUsersGender']);
+    Route::post('/device-category', [AnalyticsController::class, 'getDeviceCategory']);
+
+    Route::post('/interests-other', [AnalyticsController::class, 'getInterestsOther']);
+    Route::post('/interests-market', [AnalyticsController::class, 'getInterestsMarket']);
+    Route::post('/interests-affinity', [AnalyticsController::class, 'getInterestsAffinity']);
 });
